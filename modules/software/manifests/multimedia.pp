51ce75c6e7fd6074fae35d091d79a95cdcc9c543
Brian Pitts <brian@polibyte.com>
2012-02-03 16:44:02 -0500

    Add multimedia software

---
+++ modules/software/manifests/multimedia.pp
@@ -0,0 +1,10 @@
+class software::multimedia {
+
+  package { ['lastfm']: }
+  package { ['mplayer', 'gnome-mplayer']: }
+  package { ['picard']: }
+  package { ['quodlibet', 'quodlibet-plugins', 'gstreamer0.10-alsa', 'python-indicate']: }
+  package { ['shotwell']: }
+  package { ['transmission-gtk']: }
+
+}
