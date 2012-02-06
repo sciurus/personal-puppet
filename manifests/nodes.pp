0b241f2606cdfc96f175e449a01df66f2f5749f4
Brian Pitts <brian@polibyte.com>
2012-02-06 14:49:49 -0500

    Add remaining nodes

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,8 +1,20 @@
 node 'portege' {
-
   include apt
   include laptop
   include software
   class { user: name => 'brian' }
+}
 
+node 'triangle' {
+  include apt
+  include laptop
+  include software
+  include software::multimedia
+  class { user: name => 'brian' }
+}
+
+node 'date.ctegd.uga.edu' {
+  include apt
+  include software
+  class { user: name => 'bdp' }
 }
