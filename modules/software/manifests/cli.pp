c887fa968db7a5029c4a6fa21281741668d2d56f
Brian Pitts <brian@polibyte.com>
2012-02-20 14:47:28 -0500

    Add vnc viewer

--- modules/software/manifests/cli.pp
+++ modules/software/manifests/cli.pp
@@ -2,7 +2,7 @@ class software::cli {
   # cli apps
 
   # ones with no dependencies
-  $cli=['apg', 'apt-file', 'apt-rdepends', 'atop', 'cpufrequtils', 'cryptsetup', 'curl', 'duplicity', 'genisoimage', 'git', 'inotify-tools', 'markdown', 'mtr', 'nmap', 'pv', 'rsnapshot', 'rsync', 'screen', 'sshuttle', 'subversion', 'strace', 'tshark', 'vim', 'wodim']
+  $cli=['apg', 'apt-file', 'apt-rdepends', 'atop', 'cpufrequtils', 'cryptsetup', 'curl', 'duplicity', 'genisoimage', 'git', 'inotify-tools', 'markdown', 'mtr', 'nmap', 'pv', 'rsnapshot', 'rsync', 'screen', 'sshuttle', 'subversion', 'strace', 'tshark', 'vim', 'xtightvncviewer', 'wodim']
 
   package { $cli: }
 
